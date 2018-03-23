55f0a65eb9911da11168062c9db43306d5d706f3
haxck <haxck.x@gmail.com>
2018-03-23 09:37:10 +0800

    完成前端获取页面

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,20 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("wp-json/wp/v2/posts/")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <header className="news-list-nav">
+          <a href="#">US...</a>
+          </header>
+          <div className="news-list-view">
+            <ul className="news-list">
+              {items.map(item => (
+                <li key={item.id} className="news-item">
+                <span className="item-title">
+                <a href={"http://localhost:8000/api/p/" + item.link} target="_blank">{item.id}:{item.title.rendered}</a>
+                </span>
+                <br/>
+                <span className="item-time">
+                  {item.date}
+                </span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
